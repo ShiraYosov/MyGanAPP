de0f672392f697a6ad9902bd68a898d733a1aa9d
ShiraYosov <Shira.yosov@gmail.com>
2021-10-16 15:52:40 +0300

    .

---
+++ MyGanAPP/Models/Allergy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyGanAPP.Models
+{
+    public partial class Allergy
+    {
+        public Allergy()
+        {
+            StudentAllergies = new HashSet<StudentAllergy>();
+        }
+
+        public int AllergyId { get; set; }
+        public string AllergyName { get; set; }
+
+        public virtual ICollection<StudentAllergy> StudentAllergies { get; set; }
+    }
+}
