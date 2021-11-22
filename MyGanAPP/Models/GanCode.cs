b8e5758c67f473678b987d7e49faf4cfd01610bb
User <User@LAB3-1>
2021-11-22 12:12:11 +0200

    class gan code

--- MyGanAPP/Models/GanCode.cs
+++ MyGanAPP/Models/GanCode.cs
@@ -6,20 +6,32 @@ namespace MyGanAPP.Models
 {
     class GanCode
     {
-        public int KindergartenID { get; set; }
-        public string Code;
+        private const int HASHCODE = 9973;
 
-        public string CreateGanCode()
+        public static string CreateGanCode(int ID)
         {
-            Code = "G" + $"{ App.Code / KindergartenID}" + "N" + $" {App.Code % KindergartenID}";
-            return Code;
+           return "G" + $"{HASHCODE / ID}" + "N" + $" {HASHCODE % ID}";
+            
         }
 
-        public int CodeToGanID()
+        public static int CodeToGanID(string code)
         {
+            int gPos = code.IndexOf('G');
+            int nPos = code.IndexOf('N');
 
-            
-            return 0;
+            if (gPos != 0 || nPos <= 2 )
+                return 0;
+
+            string param1 = code.Substring(1, nPos - 1);
+            string param2 = code.Substring(nPos);
+
+            int p1 = 0, p2 = 0;
+            if (!int.TryParse(param1, out p1) ||
+                !int.TryParse(param2, out p2))
+                return 0;
+
+            return (HASHCODE - p2 ) / p1;
+
         }
     }
 }
