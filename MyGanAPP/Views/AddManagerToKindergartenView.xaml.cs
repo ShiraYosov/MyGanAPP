af543398689eed2039e7c7a6a40caa5c0bbcb305
User <User@LAB3-2>
2022-03-23 10:21:59 +0200

    :(

---
+++ MyGanAPP/Views/AddManagerToKindergartenView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGanAPP.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Services;
+
+namespace MyGanAPP.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddManagerToKindergartenView : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public AddManagerToKindergartenView(object context)
+        {
+            this.BindingContext = context;
+            InitializeComponent();
+        }
+    }
+}
